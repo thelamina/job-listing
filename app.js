94d1c1194843d8f4968b5055595993f8712f1790
Itunu Lamina <Nix4phun@gmail.com>
2020-07-02 22:59:38 +0100

    CSS WIP

--- app.js
+++ app.js
@@ -8,13 +8,16 @@ class App {
     return data;
   }
 
+  showSearch(){
+  }
+  
   showJobs() {
     this.getData().then((jobs) => {
       let output = '';
       jobs.forEach((job) => {
         output += `
           <div class="card-item">
-            <div class="details">
+          <div class="details">
               <img src="${job.logo}" alt="" />
               <div>
                 <div class="company">
@@ -25,21 +28,21 @@ class App {
                       ? '<span class="badge bg-dark">Featured</span>'
                       : ''
                   }  
-                </div>
+                  </div>
                 <a href="#" class="position">
                   ${job.position}
-                </a>
+                  </a>
                 <div class="desciption">
                   <p class="date">${job.postedAt}</p>
                   <p class="contract">${job.contract}</p>
                   <p class="location">${job.location}</p>
-                </div>
+                  </div>
               </div>
             </div>
             <div class="tag">
-              <span class="role">${job.role}</span>
+            <span class="role">${job.role}</span>
               <span class="level">${job.contract}</span>
-
+              
               ${job.tools.map((lang) => {
                 return '<span class="tool">' + lang + '</span>';
               })}
@@ -48,37 +51,52 @@ class App {
                 return '<span class="language">' + lang + '</span>';
               })}
               
-            </div>
+              </div>
           </div>
           `;
-      });
+        });
       document.querySelector('.lists').innerHTML = output;
     });
   }
 
-  makeActive(){
+  makeActive() {
     console.log('I am here');
     filter.forEach((text) => {
       // text.parentElement.classList += ' current';
       console.log(text);
     });
-    console.log(filter)
+    console.log(filter);
   }
 
   addToFilter(e) {
     if (
       e.target.classList.contains('tool') ||
-      e.target.classList.contains('language')
+      e.target.classList.contains('language') ||
+      e.target.classList.contains('role') ||
+      e.target.classList.contains('level')
     ) {
-      if (!filter.includes(e.target.textContent)) {
-        filter.push(e.target.textContent);
-        ()=>this.makeActive();
+      if (!filter.includes(e.target.innerText)) {
+        filter.push(e.target.innerText);
+      } else {
+        filter.splice(filter.indexOf(e.target.innerText), 1);
       }
-      console.log(filter);
     }
+    document.querySelectorAll('.tag>span').forEach((el) => {
+      filter.map((elem) => {
+        if (el.innerText === elem) {
+          el.classList.toggle('current');
+        }
+      });
+    });
+    console.log(filter);
+    if (filter.length!==0) {
+      document.querySelector('.search-bar').style.display = 'block';
+    }
+    // console.log(filter);
   }
 }
 
 const Init = new App();
 Init.showJobs();
+Init.showSearch();
 document.body.addEventListener('click', Init.addToFilter);
